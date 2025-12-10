24f8751b9169e86dae9b0c60071af0ea7eeb350d
alexandra190220 <maria.alexandra.morales@correounivalle.edu.co>
2025-12-10 13:27:27 -0500

    modifique profesorService

--- src/services/profesorService.js
+++ src/services/profesorService.js
@@ -51,7 +51,7 @@ export const profesorService = {
 
       // Usar fetch directamente para FormData
       const token = localStorage.getItem('accessToken');
-      const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+      const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://marcocastr0.github.io/GHP_SASCE/';
       
       const response = await fetch(`${API_BASE_URL}/coordinador/profesores`, {
         method: 'POST',
